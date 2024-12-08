710955eed9148abd167933fa9f7289601870b98d
LizaZalozna <liza.zalozna@gmail.com>
2024-12-08 14:39:23 +0200

    Circle

--- lab_polymorphism/Class1.cs
+++ lab_polymorphism/Class1.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using lab_polymorphism;
+using System;
 using System.Threading;
 using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
 
 abstract class Figure
 {
-	private int xCenter;
-	private int yCenter;
-    private Form form;
+	protected int xCenter;
+	protected int yCenter;
+    protected Form form;
 
     public void MoveRight()
     {
@@ -15,10 +17,34 @@ abstract class Figure
             DrawBlack();
             Thread.Sleep(100);
             HideDrawingBackGround();
-            xCenter += 10;
+            xCenter += 5;
         }
     }
 
     public abstract void DrawBlack();
     public abstract void HideDrawingBackGround();
+}
+
+class Circle:Figure
+{
+    private int radius;
+
+    public Circle(int xCenter, int yCenter, int radius, Form form)
+    {
+        this.xCenter = xCenter;
+        this.yCenter = yCenter;
+        this.radius = radius;
+        this.form = form;
+    }
+    public override void DrawBlack()
+    {
+        Graphics graphics = form.CreateGraphics();
+        graphics.DrawEllipse(Pens.Black, xCenter - radius, yCenter - radius, radius * 2, radius * 2);
+    }
+
+    public override void HideDrawingBackGround()
+    {
+        Graphics graphics = form.CreateGraphics();
+        graphics.DrawEllipse(new Pen(form.BackColor), xCenter - radius, yCenter - radius, radius * 2, radius * 2);
+    }
 }
