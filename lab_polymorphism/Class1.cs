05941357949e8dba6a710b76261472d5983746ad
LizaZalozna <liza.zalozna@gmail.com>
2024-12-08 14:12:42 +0200

    Figure

---
+++ lab_polymorphism/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+abstract class Figure
+{
+	private int xCenter;
+	private int yCenter;
+    private Form form;
+
+    public void MoveRight()
+    {
+        while (xCenter < form.Width)
+        {
+            DrawBlack();
+            Thread.Sleep(100);
+            HideDrawingBackGround();
+            xCenter += 10;
+        }
+    }
+
+    public abstract void DrawBlack();
+    public abstract void HideDrawingBackGround();
+}
