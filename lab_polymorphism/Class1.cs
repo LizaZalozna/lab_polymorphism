9907fc50f866b12fd2627e1bea3aad4b465636b8
LizaZalozna <liza.zalozna@gmail.com>
2024-12-08 14:50:03 +0200

    Square

--- lab_polymorphism/Class1.cs
+++ lab_polymorphism/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using lab_polymorphism;
 using System;
+using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
 using static System.Windows.Forms.DataFormats;
@@ -28,7 +29,6 @@ abstract class Figure
 class Circle:Figure
 {
     private int radius;
-
     public Circle(int xCenter, int yCenter, int radius, Form form)
     {
         this.xCenter = xCenter;
@@ -46,5 +46,37 @@ class Circle:Figure
     {
         Graphics graphics = form.CreateGraphics();
         graphics.DrawEllipse(new Pen(form.BackColor), xCenter - radius, yCenter - radius, radius * 2, radius * 2);
+    }
+}
+
+class Square : Figure
+{
+    private int sideLength;
+    public Square(int xCenter, int yCenter, int sideLength, Form form)
+    {
+        this.xCenter = xCenter;
+        this.yCenter = yCenter;
+        this.sideLength = sideLength;
+        this.form = form;
+    }
+    private Point[] GetPointsForOurFigure()
+    {
+        return new Point[] {
+            new Point(xCenter - sideLength/2,  yCenter - sideLength/2),
+            new Point(xCenter - sideLength/2,  yCenter + sideLength/2),
+            new Point(xCenter + sideLength/2,  yCenter + sideLength/2),
+            new Point(xCenter + sideLength/2,  yCenter - sideLength/2)
+            };
+    }
+    public override void DrawBlack()
+    { 
+        Graphics graphics = form.CreateGraphics();
+        graphics.DrawPolygon(Pens.Black, GetPointsForOurFigure());
+    }
+
+    public override void HideDrawingBackGround()
+    {
+        Graphics graphics = form.CreateGraphics();
+        graphics.DrawPolygon(new Pen(form.BackColor), GetPointsForOurFigure());
     }
 }
